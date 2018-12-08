85c1fbf36dd625a57811b758f9a77ccd1679e793
dan <danieleades@hotmail.com>
2018-12-08 09:37:21 +0000

    add 'description' field to transaction.add

--- budget-bin/src/app/transaction/add.rs
+++ budget-bin/src/app/transaction/add.rs
@@ -1,25 +1,26 @@
 use budget_lib::{Budget, Transaction};
-use clap::{App, Arg, ArgMatches, SubCommand};
+use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
 use decimal::d128;
 use std::str::FromStr;
 
 pub fn command<'a, 'b>() -> App<'a, 'b> {
-    SubCommand::with_name("add")
-        // app settings
-        // arguments
-        .arg(
-            Arg::with_name("amount")
-                .takes_value(true)
-                .help("The amount of the transaction")
-                .required(true),
-        )
+        SubCommand::with_name("add")
+                .setting(AppSettings::TrailingVarArg)
+                // app settings
+                // arguments
+                .arg(Arg::with_name("amount")
+                        .takes_value(true)
+                        .help("The amount of the transaction")
+                        .required(true))
+                .arg(Arg::with_name("description").multiple(true))
 }
 
 pub fn delegate(budget: &mut Budget, matches: &ArgMatches) {
-    process(budget, matches)
+        process(budget, matches)
 }
 
 pub fn process(budget: &mut Budget, matches: &ArgMatches) {
-    let amount = d128::from_str(matches.value_of("amount").unwrap()).unwrap();
-    budget.add(Transaction::new(amount))
+        let amount = d128::from_str(matches.value_of("amount").unwrap()).unwrap();
+        let description = matches.value_of("description");
+        budget.add(Transaction::new(amount).with_description(description))
 }
