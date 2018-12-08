0db2acd47d6d6df21cb83ab137d5553ff3e64495
dan <danieleades@hotmail.com>
2018-12-08 15:40:28 +0000

    fix transaction.add so it uses the whole description and not just the first word

--- budget-bin/src/app/transaction/add.rs
+++ budget-bin/src/app/transaction/add.rs
@@ -4,25 +4,33 @@ use decimal::d128;
 use std::str::FromStr;
 
 pub fn command<'a, 'b>() -> App<'a, 'b> {
-    SubCommand::with_name("add")
-        .setting(AppSettings::TrailingVarArg)
-        // app settings
-        // arguments
-        .arg(
-            Arg::with_name("amount")
-                .takes_value(true)
-                .help("The amount of the transaction")
-                .required(true),
-        )
-        .arg(Arg::with_name("description").multiple(true))
+        SubCommand::with_name("add")
+                .setting(AppSettings::TrailingVarArg)
+                // app settings
+                // arguments
+                .arg(Arg::with_name("amount")
+                        .takes_value(true)
+                        .help("The amount of the transaction")
+                        .required(true))
+                .arg(Arg::with_name("description").multiple(true))
 }
 
 pub fn delegate(budget: &mut Budget, matches: &ArgMatches) {
-    process(budget, matches)
+        process(budget, matches)
 }
 
 pub fn process(budget: &mut Budget, matches: &ArgMatches) {
-    let amount = d128::from_str(matches.value_of("amount").unwrap()).unwrap();
-    let description = matches.value_of("description");
-    budget.add(Transaction::new(amount).with_description(description))
+        let amount = d128::from_str(matches.value_of("amount").unwrap()).unwrap();
+        let description = matches.values_of("description").map(|mut tokens| {
+                let mut sentence = "".to_string();
+                if let Some(x) = tokens.next() {
+                        sentence.push_str(x);
+                }
+                for token in tokens {
+                        sentence += &format!(" {}", token)
+                }
+                sentence
+        });
+
+        budget.add(Transaction::new(amount).with_description(description))
 }
